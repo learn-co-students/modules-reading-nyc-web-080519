e01d2f14e2ed1013e9590c85a5bee91360ebd8af
tomjmcnamee <tom@tomjmcnamee.com>
2019-08-11 12:09:39 -0400

    Done.

---
+++ lib/dance_module.rb
@@ -0,0 +1,19 @@
+module Dance
+
+  def twirl
+    "I'm twirling!"
+  end  # ends twirl method
+ 
+  def jump
+    "Look how high I'm jumping!"
+  end  # ends jump method
+ 
+  def pirouette
+    "I'm doing a pirouette"
+  end  # ends pirouette method
+ 
+  def take_a_bow
+    "Thank you, thank you. It was a pleasure to dance for you all."
+  end  # ends take_a_bow method
+
+end  # ends Dance module
